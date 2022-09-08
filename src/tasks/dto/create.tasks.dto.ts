1a8cfa4746ab2e5f82008a3a9a44b213ae41f34c
Cpietro87 <cristianpietro2019@gmail.com>
2022-09-08 17:41:06 -0300

    manejo de datos

---
+++ src/tasks/dto/create.tasks.dto.ts
@@ -0,0 +1,5 @@
+export class createTasksDto {
+    title: string;
+    description: string;
+    done: boolean;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,26 +1,30 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { createTasksDto } from "./dto/create.tasks.dto";
 
 @Controller('tasks')
 export class TasksController {
     @Get()
-    getTasks(): string{
-        return 'Hello Tasks';
+    getTasks(): {hello: string}{
+        return {"hello": "world"};
     }
 
     @Post()
-    createTasks(@Body() task): string{
+    createTasks(@Body() task: createTasksDto): string{
         console.log(task)
         return 'creating Tasks';
     }
 
-    @Put()
-    updateTasks(): string {
+    @Put(":id")
+    updateTasks(@Body() task: createTasksDto, @Param('id') id): string {
+        console.log(task);
+        console.log(id)
         return 'update Tasks';
     }
 
-    @Delete()
-    deleteTasks(): string{
-        return 'delete Tasks';
+    @Delete(":id")
+    deleteTasks(@Param('id') id): string{
+        console.log(id);
+        return `delete Tasks number: ${id}`;
     }
       
 }
